acd948a426f8b2769374a78d61f55b434ec3ba7a
Will <doggy.huang@gmail.com>
2022-02-21 11:20:48 +0800

    調整資料存取邏輯，僅顯示 IsEnable=true 的資料

--- MVC5Course/Models/CourseRepository.cs
+++ MVC5Course/Models/CourseRepository.cs
@@ -6,8 +6,11 @@ namespace MVC5Course.Models
 {   
 	public  class CourseRepository : EFRepository<Course>, ICourseRepository
 	{
-
-	}
+        public override IQueryable<Course> All()
+        {
+            return base.All().Where(p => p.IsEnabled);
+        }
+    }
 
 	public  interface ICourseRepository : IRepository<Course>
 	{
