8666b811a0c2fb92bcd1392dbc5ed93cf3896a03
Will <doggy.huang@gmail.com>
2022-02-21 15:11:58 +0800

    練習從 Action 傳值到 View

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData.Model = "1";
+            ViewData["Key1"] = "Key1";
+            ViewBag.Key2 = "Key2";
+
+            return View();
+        }
+
+        public ActionResult TD()
+        {
+            TempData["Msg"] = "Success!";
+
+            Session["Msg2"] = "OK";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
