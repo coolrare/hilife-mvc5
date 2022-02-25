eaf72b3197ffb1419bf9e2f462033691e9646b02
Will <doggy.huang@gmail.com>
2022-02-25 15:30:27 +0800

    套用範本檢視

--- MVC5Course/Models/Course.Partial.cs
+++ MVC5Course/Models/Course.Partial.cs
@@ -33,6 +33,7 @@ namespace MVC5Course.Models
             [Display(Name = "課程評價")]
             [Required(ErrorMessage = "設定課程評價({0})為必填欄位")]
             [Range(0, 5, ErrorMessage = "課程評價僅能輸入 0 ~ 5")]
+            [UIHint("以星星顯示Credit欄位")]
             public int Credits { get; set; }
             [Display(Name = "部門名稱")]
             public int DepartmentID { get; set; }
